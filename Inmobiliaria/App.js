b8daffc9d3fbbc0484dfcab6379a8ebfe41a0d67
danthek <abraham8507@gmail.com>
2021-12-03 15:25:56 -0600

    APP DE INMOBILIARIA UPDATE UPDATE WITH MAPPING OF DATA TO AUTOMATICALLY CREATE THE TEMPLATES INSTEAD OF WRITING EVERY SINGLE ONE OF THEM.

--- Inmobiliaria/App.js
+++ Inmobiliaria/App.js
@@ -4,48 +4,64 @@
 import React from 'react';
 import {View} from 'react-native';
 import Template from './src/components/HouseTemplate';
+import data from './src/data';
 
 const App = () => {
-  return (
-    <View>
+  const newTemplateList = data.map(
+    ({name, direction, beds, baths, sqfeets, price, housePic}, index) => (
       <Template
-        name="The Willows"
-        direction="3517 W. Gray St. Utica"
-        beds="3"
-        baths="2"
-        sqfeets="230"
-        price="440"
-        housePic={require('../Inmobiliaria/src/imgs/house-aubrey.jpg')}
+        key={index}
+        name={name}
+        direction={direction}
+        beds={beds}
+        baths={baths}
+        sqfeets={sqfeets}
+        price={price}
+        housePic={housePic}
       />
-      <Template
-        name="Quintana Roo"
-        direction="253 Fco. I. Madero Cancún"
-        beds="4"
-        baths="3"
-        sqfeets="755"
-        price="975"
-        housePic={require('../Inmobiliaria/src/imgs/beach-house.jpg')}
-      />
-      <Template
-        name="Cuernavaca"
-        direction="2864 Diaz Mirón Morelos"
-        beds="5"
-        baths="2"
-        sqfeets="489"
-        price="832"
-        housePic={require('../Inmobiliaria/src/imgs/cabin.jpg')}
-      />
-      <Template
-        name="Guanajuato"
-        direction="1438 Lapis Lazulli Gto."
-        beds="4"
-        baths="3"
-        sqfeets="389"
-        price="1979"
-        housePic={require('../Inmobiliaria/src/imgs/mexican-house.jpg')}
-      />
-    </View>
+    ),
   );
+
+  return <View>{newTemplateList}</View>;
 };
 
 export default App;
+
+{
+  /* <Template
+  name="The Willows"
+  direction="3517 W. Gray St. Utica"
+  beds="3"
+  baths="2"
+  sqfeets="230"
+  price="440"
+  housePic={require('../Inmobiliaria/src/imgs/house-aubrey.jpg')}
+/>
+<Template
+  name="Quintana Roo"
+  direction="253 Fco. I. Madero Cancún"
+  beds="4"
+  baths="3"
+  sqfeets="755"
+  price="975"
+  housePic={require('../Inmobiliaria/src/imgs/beach-house.jpg')}
+/>
+<Template
+  name="Cuernavaca"
+  direction="2864 Diaz Mirón Morelos"
+  beds="5"
+  baths="2"
+  sqfeets="489"
+  price="832"
+  housePic={require('../Inmobiliaria/src/imgs/cabin.jpg')}
+/>
+<Template
+  name="Guanajuato"
+  direction="1438 Lapis Lazulli Gto."
+  beds="4"
+  baths="3"
+  sqfeets="389"
+  price="1979"
+  housePic={require('../Inmobiliaria/src/imgs/mexican-house.jpg')}
+/> */
+}
---
+++ Inmobiliaria/src/data.js
@@ -0,0 +1,49 @@
+import Image1 from '../src/imgs/house-aubrey.jpg';
+import Image2 from '../src/imgs/beach-house.jpg';
+import Image3 from '../src/imgs/cabin.jpg';
+import Image4 from '../src/imgs/mexican-house.jpg';
+
+const data = [
+  {
+    //INDEX 0
+    name: 'The Willows',
+    direction: '3517 W. Gray St. Utica',
+    beds: '3',
+    baths: '2',
+    sqfeets: '230',
+    price: '440',
+    housePic: Image1,
+  },
+  {
+    //INDEX 1
+    name: 'Quintana Roo',
+    direction: '253 Fco. I. Madero Cancún',
+    beds: '4',
+    baths: '3',
+    sqfeets: '755',
+    price: '975',
+    housePic: Image2,
+  },
+  {
+    // INDEX 2
+    name: 'Cuernavaca',
+    direction: '2864 Diaz Mirón Morelos',
+    beds: '5',
+    baths: '2',
+    sqfeets: '489',
+    price: '832',
+    housePic: Image3,
+  },
+  {
+    //INDEX3
+    name: 'Guanajuato',
+    direction: '1438 Lapis Lazulli Gto.',
+    beds: '4',
+    baths: '3',
+    sqfeets: '389',
+    price: '1979',
+    housePic: Image4,
+  },
+];
+
+export default data;
